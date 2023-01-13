6b69d0a353e9514f90a66a26f4e6e737aa33057e
Chaelin <dhcofls123@daum.net>
2023-01-13 16:42:47 +0900

    프로필인데 displayname 오류

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,16 +1,44 @@
-import { authService } from '../fBase';
-import React from 'react';
+import { authService, dbService } from '../fBase';
+import {useState} from "react";
+import React, { useEffect } from 'react';
 import {useNavigate} from "react-router-dom";
+import {collection, getDocs, orderBy, query, where} from "@firebase/firestore";
+import {getAuth, signOut} from "firebase/auth";
+import {updateProfile} from "@firebase/auth";
 
-export default () => {
+const Profile =({userObj}) => {
     const history = useNavigate();
-     const onLogOutClick = () => {
+    const [newDisplayName, setNewDisplayName] = useState(userObj.dispalyName);
+    const auth = getAuth(); 
+    const onLogOutClick = () => {
         authService.signOut()
         history.push("/");
      };
-    return (
+
+     const onChange = (event) => {
+        const {
+            target: {value},
+        } = event;
+        setNewDisplayName(value);
+    };
+        const onSubmit = async (event) => {
+            event.preventDefault();
+            if(userObj.displayName !== newDisplayName){
+                await updateProfile(userObj, {displayName: newDisplayName});
+            }
+        };
+
+     return (
         <>
-            <button onClick={onLogOutClick}>Log Out</button>
+        <form onSubmit={onSubmit}>
+            <input onChange={onChange} type="text" placholder="Display name" value={newDisplayName} />
+            <input type="submit" value="Update Profile"/>
+        </form>
+        <button onClick={onLogOutClick}>Log Out</button>
         </>
-        );
+     )
+
+
 };
+
+export default Profile;
